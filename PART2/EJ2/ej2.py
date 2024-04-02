b0a00119e4c4d4e5ac642292e3f05d05f893c7f2
fmonpelat <facundo.monpelat@gmail.com>
2024-04-02 19:46:08 -0300

    complejidades del ej2 y intro a la resolucion

--- PART2/EJ2/ej2.py
+++ PART2/EJ2/ej2.py
@@ -26,25 +26,35 @@ Se pide:
 
 #RESOLUCIÓN  *****************************************************************************************************
 """
-    EJEMPLO 1
-                            ---------A-----
+Identificamos que al ser un arbol jerarquico, tendremos una estructura de piramide mientras mas profundo sea el arbol. Con esta idea entendemos que es mejor arrancar agregando
+las hojas del arbol antes que el nodo raiz. 
+Por lo tanto, se propone el siguiente algoritmo greedy:
+    1- Realizar un recorrido BFS para obtener los nodos en orden de jerarquia.
+    2- Invertir la lista obtenida en el paso 1. Para que los nodos mas profundos esten al principio.
+    3- Recorrer la lista invertida y agregar a los nodos que no esten en la lista de excluidos. Si el nodo padre esta en la lista de excluidos, no se agrega.
+    
+Otra forma seria realizar el recorrido bfs y no invertir la lista. Pero se considera que es mejor empezar por las hojas, ya que haciendo pruebas sobre el ejemplo 1, se obtiene una cantidad
+inferior de invitados.
+
+    EJEMPLO 1:
+                            --------A------
                             |     |    |  |
                           --B--  -C-   D  E
                           | | |  | |   |
                           F G H  I J  -K-
-                                      |||      
+                                      |||
                                       LMN 
 
 
-    1 BFS = A B C D E F G H I J K L M N
-    2 INVERTIR = N M L K J I H G F E D C B A
-    3 agarrar primero, revisar si es evitar: 
-            N = evitar? NO ---> agrego INVITADOS = [N]
-            evitar padre N---> K: EVITAR = [K]
+    1. Realizar recorrido BFS = A B C D E F G H I J K L M N
+    2. Invertir la lista = N M L K J I H G F E D C B A
+    3. Agarrar el primero, revisar si es evitar: 
+            N = evitar? NO ---> agrego INVITADOS = [ N ]
+            evitar padre N---> K: EVITAR = [ K ]
             ....
-    4 resultado= [F G H  I J L M N E D]
+    4. Resultado = [ F G H  I J L M N E D ]
 
-    ejemplo 2:
+    EJEMPLO 2:
                                     
                             ---------A-----
                             |     |    |  |
@@ -53,20 +63,30 @@ Se pide:
                                  F G   H
                                  |
                                  I
-    1 BFS = A B C D E F G H I 
-    2 INVERTIR = I H G F E D C B A
-    3 agarrar primero, revisar si es evitar: 
+    1. Realizar recorrido BFS = A B C D E F G H I 
+    2. Invertir la lista = I H G F E D C B A
+    3. Agarrar el primero, revisar si es evitar:
             ...
 
-    4 Resultado = [I F H B E]
-       
+    4. Resultado = [ I F H B E ]
+
+
+Analisis de complejidad temporal
+    - del Recorrido BFS tenemos O(V+E)
+    - Como mejora no se invirtio la lista y se la recorrio de atras a adelante, el recorrido de la lista es O(V)
+    - Total: O(V+E)
+
+Analisis de complejidad espacial
+    - para el listado_bfs se utiliza O(V)
+    - para los conjuntos invitados y no_invitar se utiliza O(V) entre ambos
+    - Total: O(V)
 """
 
 
 
 
 # ALGORITMO: *****************************************************************************************************
-def obtener_maximos_invitados_trabajo_bfs(organigrama_laboral,jefe):
+def obtener_maximos_invitados_trabajo_bfs(organigrama_laboral,jefe) -> set:
     listado_bfs = []
     armado_lista_preorder(jefe,None,listado_bfs,organigrama_laboral)
     invitados = set()
@@ -151,13 +171,13 @@ def armar_arbol_ejemplo2():
     
 def main():
     
-    jefe = "A"
+    ceo = "A"
 
     organigrama_laboral_1 = armar_arbol_ejemplo1()
-    print(obtener_maximos_invitados_trabajo_bfs(organigrama_laboral_1,jefe)) # {'J', 'L', 'M', 'H', 'N', 'I', 'F', 'G', 'D', 'E'} parece funcionar
+    print(obtener_maximos_invitados_trabajo_bfs(organigrama_laboral_1, ceo)) # {'J', 'L', 'M', 'H', 'N', 'I', 'F', 'G', 'D', 'E'} parece funcionar
 
     organigrama_laboral_2 = armar_arbol_ejemplo2()
-    print(obtener_maximos_invitados_trabajo_bfs(organigrama_laboral_2,jefe)) # {'B','I','G','H','E'} parece funcionar
+    print(obtener_maximos_invitados_trabajo_bfs(organigrama_laboral_2, ceo)) # {'B','I','G','H','E'} parece funcionar
 
 
 main()
